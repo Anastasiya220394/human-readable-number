d2ffdc875d2bb7fdafab8353fff0b45ba7eee9e3
Anastasiya Nitievskaya <anonim22@icloud.com>
2021-08-02 15:48:39 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+let a = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+let b = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+
 module.exports = function toReadable (number) {
-  
+    if (number < 20) return a[number];
+    let result = number%10;
+    if (number < 100) return b[~~(number/10)-2] + (result? " " + a[result]: "");
+    if (number < 1000) return a[~~(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100));
 }
